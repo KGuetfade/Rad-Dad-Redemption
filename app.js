4bd266d3203b68223b548bb8b6d318a51d2cb6fb
KGuetfade <karim.guettache@gmail.com>
2018-12-04 15:58:52 +0100

    splash.html

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const websocket = require("ws");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + '/public')));
 
- app.get("/", function(req, res) {
+app.get("/", function(req, res) {
       res.statusCode = 200;
+      res.sendFile("splash.html", {root: "./public"});
 })
 
  app.listen(port, function(){
