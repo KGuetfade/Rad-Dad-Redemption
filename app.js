9044eb67f87f9b8e8707a5532f0f3f8565bac620
KGuetfade <karim.guettache@gmail.com>
2018-12-04 15:40:32 +0100

    added websocket package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const websocket = require("ws");
 
 const app = express();
 const port = 3000;
@@ -11,10 +12,7 @@ app.use(express.static(path.join(__dirname + '/public')));
  app.get("/", function(req, res) {
       res.statusCode = 200;
 })
-    
+
  app.listen(port, function(){
     console.log("Server Started on Port " + port);
 });
-
-
-
