e09f017ca096fa483786b83219a6e222ddedc433
SDuveen <semduveen@gmail.com>
2018-12-04 18:12:06 +0100

    counter

--- public/javascripts/interactions.js
+++ public/javascripts/interactions.js
@@ -7,3 +7,34 @@ $(function(){
         }
     });
 });
+
+
+$(function(){
+    var seconds = 60;
+    var minutes = 4;
+
+   function decMin(){   
+       minutes--;
+       document.getElementById("time").innerHTML = minutes + ":" + seconds;
+   }
+
+   function decSec(){
+    if(seconds === 0) { 
+        document.getElementById("time").innerHTML = minutes + ":0" + seconds;
+        seconds = 59;
+    }
+    else {
+        seconds = seconds - 1;
+        if(seconds < 10){
+            document.getElementById("time").innerHTML = minutes + ":0" + seconds;
+        }
+        else{
+            document.getElementById("time").innerHTML = minutes + ":" + seconds;
+        }
+    }
+   }
+
+   var x = setInterval(decSec, 1000);
+   var y = setInterval(decMin, 61000);
+
+});
