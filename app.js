925e35e22c0c74b69f4c20fd174a7855119dc662
SDuveen <semduveen@gmail.com>
2018-12-04 14:56:11 +0100

    nou hij werkt hoor

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const http = require("http");
-const cookies = require("cookie-parser");
-const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-fs.readFile('splash.html', (err, html) => {
-    if(err){
-        throw err;
-    }
-    app.get("/", function(req, res) {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
-        res.write(html);
-        res.end();
-    })
-    
-    app.listen(port, function(){
-        console.log("Server Started on Port " + port);
-    });
+app.use(express.static(path.join(__dirname + '/public')));
+
+ app.get("/", function(req, res) {
+      res.statusCode = 200;
 })
+    
+ app.listen(port, function(){
+    console.log("Server Started on Port " + port);
+});
+
+
 
