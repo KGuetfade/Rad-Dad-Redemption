c1904c32f19d8efac3c83f3631043c0788dd7ac1
KGuetfade <karim.guettache@gmail.com>
2018-12-05 15:09:08 +0100

    A

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -11,3 +11,14 @@ socket.onopen = function(event){
 socket.onmessage = function(event){
     alert(event.data);
 }
+
+var player = function(playerID){
+    this.ID = playerID;
+    this.board = [[],[]]; //10x10
+    this.ships = 0;
+    this.gamestate = 0;
+}
+
+player.prototype.Shoot = function(){
+    //shoot on other board and then send the coordinates to server
+}
